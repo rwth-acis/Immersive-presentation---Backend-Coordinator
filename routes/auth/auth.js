fae01528ed9216202bc87a652f89f781e1009d75
Lukas Liss <lukas.liss@rwth-aachen.de>
2021-01-09 19:47:41 +0100

    openidconnect

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -13,161 +13,252 @@ const mailer = require("../../tools/mailer");
 //
 /* POST register user (doctor / patient) */
 router.post("/register/", function (req, res, next) {
-    //VALIDATION
-    req.checkBody(
-        "email",
-        "The email you entered is invalid, please try again."
+  //VALIDATION
+  req
+    .checkBody("email", "The email you entered is invalid, please try again.")
+    .isEmail()
+    .normalizeEmail();
+  req
+    .checkBody(
+      "email",
+      "Email address must be between 4-100 characters long, please try again."
     )
-        .isEmail()
-        .normalizeEmail();
-    req.checkBody(
-        "email",
-        "Email address must be between 4-100 characters long, please try again."
-    ).len(4, 100);
-    req.checkBody(
-        "password",
-        "Password must be between 6-50 characters long."
-    ).len(6, 50);
-    //req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+    .len(4, 100);
+  req
+    .checkBody("password", "Password must be between 6-50 characters long.")
+    .len(6, 50);
+  //req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
 
-    const errors = req.validationErrors();
+  const errors = req.validationErrors();
 
-    if (errors) {
-        res.status(400);
-        res.send({code: "#A001", message: "Validation Error", list: errors});
-    } else {
-        //Valid Data
-        const email = req.body.email;
-        const password = req.body.password;
+  if (errors) {
+    res.status(400);
+    res.send({ code: "#A001", message: "Validation Error", list: errors });
+  } else {
+    //Valid Data
+    const email = req.body.email;
+    const password = req.body.password;
 
-        // Store user in the databse
-        mysqlpool.getConnection(function (err, conn) {
-            if (err) {
-                console.log(err);
-                throw err;
-            } else {
-                //Check if the user already exists
-                var sql_statement = "SELECT EXISTS(SELECT email FROM user WHERE email = ?) AS isexisting";
-                conn.query(sql_statement, [email], function (err, result) {
-                    //No conn.release() here because it is used later
+    // Store user in the databse
+    mysqlpool.getConnection(function (err, conn) {
+      if (err) {
+        console.log(err);
+        throw err;
+      } else {
+        //Check if the user already exists
+        var sql_statement =
+          "SELECT EXISTS(SELECT email FROM user WHERE email = ?) AS isexisting";
+        conn.query(sql_statement, [email], function (err, result) {
+          //No conn.release() here because it is used later
+          if (err) {
+            conn.release();
+            res.status(500);
+            res.send({ code: "#I001", message: "MySQL-Connection-Failed" });
+            console.log(err);
+            throw err;
+            return;
+          } else {
+            if (result[0].isexisting == 0) {
+              //User does not exist
+              //Store new user in database
+
+              //Hash the password
+              bcrypt.genSalt(saltRounds, function (err, salt) {
+                if (err) {
+                  conn.release();
+                  console.log(err);
+                  res.status(500);
+                  res.send({
+                    code: "#I002",
+                    message: "Password lead to hashing error.",
+                  });
+                } else {
+                  bcrypt.hash(password, salt, function (err, hash) {
                     if (err) {
-                        conn.release();
-                        res.status(500);
-                        res.send({code: "#I001", message: "MySQL-Connection-Failed"});
-                        console.log(err);
-                        throw err;
-                        return;
+                      conn.release();
+                      console.log(err);
+                      res.status(500);
+                      res.send({
+                        code: "#I002",
+                        message: "Password lead to hashing error.",
+                      });
                     } else {
-                        if (result[0].isexisting == 0) {
-                            //User does not exist
-                            //Store new user in database
-                           
-                                //Hash the password
-                                bcrypt.genSalt(saltRounds, function (err, salt) {
-                                    if (err) {
-                                        conn.release();
-                                        console.log(err);
-                                        res.status(500);
-                                        res.send(
-                                            {code: "#I002", message: "Password lead to hashing error."}
-                                        );
-                                    } else {
-                                        bcrypt.hash(password, salt, function (
-                                            err,
-                                            hash
-                                        ) {
-                                            if (err) {
-                                                conn.release();
-                                                console.log(err);
-                                                res.status(500);
-                                                res.send(
-                                                    {code: "#I002", message: "Password lead to hashing error."}
-                                                );
-                                            } else {
-                                                //Generate the Insert statement with hashed password
-                                                sql_statement =
-                                                    "INSERT INTO user (email, pwdhash) VALUES (?, ?);";
-                                                conn.query(
-                                                    sql_statement,
-                                                    [email, hash],
-                                                    function (err, result) {
-                                                        //No conn.release() here because it is used later
-                                                        if (err) {
-                                                            conn.release();
-                                                            throw err;
-                                                        }
-                                                        //Get the user to return him
-                                                        sql_statement =
-                                                            "SELECT * FROM user WHERE email = ?";
-                                                        conn.query(
-                                                            sql_statement,
-                                                            [email],
-                                                            function (err, result) {
-                                                                conn.release();
-                                                                if (err) throw err;
-                                                                var user = cleaner.userSqlCleaner(
-                                                                    result
-                                                                );
-                                                                //expires in 1 day
-                                                                let exp =
-                                                                    Date.now() +
-                                                                    1000 * 1 * 24 * 3600;
-                                                                const token = jwt.sign(
-                                                                    { user, exp: exp },
-                                                                    jwtSecret
-                                                                );
-
-                                                                //Return
-                                                                return res.json({
-                                                                    user,
-                                                                    token,
-                                                                    exp
-                                                                });
-                                                            }
-                                                        );
-                                                    }
-                                                );
-                                            }
-                                        });
-                                    }
-                                });
-                            
-                        } else {
+                      //Generate the Insert statement with hashed password
+                      sql_statement =
+                        "INSERT INTO user (email, pwdhash) VALUES (?, ?);";
+                      conn.query(
+                        sql_statement,
+                        [email, hash],
+                        function (err, result) {
+                          //No conn.release() here because it is used later
+                          if (err) {
                             conn.release();
-                            //User already exist
-                            res.status(400);
-                            res.send({code: "#A002", message: "User already exists."});
+                            throw err;
+                          }
+                          //Get the user to return him
+                          sql_statement = "SELECT * FROM user WHERE email = ?";
+                          conn.query(
+                            sql_statement,
+                            [email],
+                            function (err, result) {
+                              conn.release();
+                              if (err) throw err;
+                              var user = cleaner.userSqlCleaner(result);
+                              //expires in 1 day
+                              let exp = Date.now() + 1000 * 1 * 24 * 3600;
+                              const token = jwt.sign(
+                                { user, exp: exp },
+                                jwtSecret
+                              );
+
+                              //Return
+                              return res.json({
+                                user,
+                                token,
+                                exp,
+                              });
+                            }
+                          );
                         }
+                      );
                     }
-                });
+                  });
+                }
+              });
+            } else {
+              conn.release();
+              //User already exist
+              res.status(400);
+              res.send({ code: "#A002", message: "User already exists." });
             }
+          }
         });
-    }
+      }
+    });
+  }
+});
+
+/* POST authenticateOIDC register or logins users that use open id connect */
+router.post("/openid/", function (req, res, next) {
+  //VALIDATION
+  req
+    .checkBody("email", "The email you entered is invalid, please try again.")
+    .isEmail()
+    .normalizeEmail();
+  req
+    .checkBody(
+      "email",
+      "Email address must be between 4-100 characters long, please try again."
+    )
+    .len(4, 100);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.status(400);
+    res.send({ code: "#A001", message: "Validation Error", list: errors });
+  } else {
+    //Valid Data
+    const email = req.body.email;
+
+    // Store user in the databse
+    mysqlpool.getConnection(function (err, conn) {
+      if (err) {
+        console.log(err);
+        throw err;
+      } else {
+        //Check if the user already exists
+        var sql_statement =
+          "SELECT EXISTS(SELECT email FROM user WHERE email = ?) AS isexisting";
+        conn.query(sql_statement, [email], function (err, result) {
+          //No conn.release() here because it is used later
+          if (err) {
+            conn.release();
+            res.status(500);
+            res.send({ code: "#I001", message: "MySQL-Connection-Failed" });
+            console.log(err);
+            throw err;
+            return;
+          } else {
+            if (result[0].isexisting == 0) {
+              //User does not exist
+              //Store new user in database
+              //Generate the Insert statement with hashed password
+              sql_statement =
+                "INSERT INTO user (email) VALUES (?);";
+              conn.query(sql_statement, [email], function (err, result) {
+                //No conn.release() here because it is used later
+                if (err) {
+                  conn.release();
+                  throw err;
+                }
+                //Get the user to return him
+                sql_statement = "SELECT * FROM user WHERE email = ?";
+                conn.query(sql_statement, [email], function (err, result) {
+                  conn.release();
+                  if (err) throw err;
+                  var user = cleaner.userSqlCleaner(result);
+                  //expires in 1 day
+                  let exp = Date.now() + 1000 * 1 * 24 * 3600;
+                  const token = jwt.sign({ user, exp: exp }, jwtSecret);
+
+                  //Return
+                  return res.json({
+                    user,
+                    token,
+                    exp,
+                  });
+                });
+              });
+            } else {
+              //User already exist
+              //Get the user to return him
+              sql_statement = "SELECT * FROM user WHERE email = ?";
+              conn.query(sql_statement, [email], function (err, result) {
+                conn.release();
+                if (err) throw err;
+                var user = cleaner.userSqlCleaner(result);
+                //expires in 1 day
+                let exp = Date.now() + 1000 * 1 * 24 * 3600;
+                const token = jwt.sign({ user, exp: exp }, jwtSecret);
+
+                //Return
+                return res.json({
+                  user,
+                  token,
+                  exp,
+                });
+              });
+            }
+          }
+        });
+      }
+    });
+  }
 });
 
 /* POST login. */
 router.post("/login/", function (req, res, next) {
-    passport.authenticate("local", { session: false }, (err, user, info) => {
-        if (err || !user) {
-            //console.log(err);
-            return res.status(400).json({
-                code: "#A003",
-                message: "Invalid Login"
-            });
-        }
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      //console.log(err);
+      return res.status(400).json({
+        code: "#A003",
+        message: "Invalid Login",
+      });
+    }
 
-        req.login(user, { session: false }, err => {
-            if (err) {
-                //console.log(err);
-                res.send({code: "A003", message: err.message});
-            }
-            //expires in a day
-            let exp = Date.now() + 1000 * 1 * 24 * 3600;
-            const token = jwt.sign({ user, exp: exp }, jwtSecret);
-            return res.json({ user, token, exp: exp });
-        });
-    })(req, res);
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        //console.log(err);
+        res.send({ code: "A003", message: err.message });
+      }
+      //expires in a day
+      let exp = Date.now() + 1000 * 1 * 24 * 3600;
+      const token = jwt.sign({ user, exp: exp }, jwtSecret);
+      return res.json({ user, token, exp: exp });
+    });
+  })(req, res);
 });
-
 
 module.exports = router;
